271961f3d4bb139ceb9f69a21bb86aa5d3e847b8
ZMR <2417894482@qq.com>
2020-01-14 01:12:33 +0000

    中间件防刷

--- app/Http/Middleware/fangshua.php
+++ app/Http/Middleware/fangshua.php
@@ -21,8 +21,8 @@ class fangshua
         {
             $redis_key = 'str:count:u' . $_SERVER['HTTP_TOKEN'] . ':url:' . $_SERVER['REQUEST_URI'];
             $count = Redis::get($redis_key);
-            if($count >= 5){
-                Redis::expire($redis_key,5);
+            if($count >= 2){
+                Redis::expire($redis_key,2);
                 $response = [
                     'errno'     => 400004,
                     'msg'       => "接口请求已达上限，请稍后再试"
